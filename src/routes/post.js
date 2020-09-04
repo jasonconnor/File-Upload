3d71845d5448d65091ba1d6298ce3cffb370d346
Jason <jasonpconn@gmail.com>
2020-09-03 22:54:52 -0400

    Updated Post Router

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const multer = require('multer')
 
 const controller = require('../controllers/post')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/jpeg') {
+      cb(null, false)
+    } else {
+      cb(null, true)
+    }
+  }
+})
 
 const router = express.Router()
 
 router.get('/', controller.index)
 
-router.post('/new', controller.create)
+router.post('/new', upload.single('image'), controller.create)
 
 module.exports = router
