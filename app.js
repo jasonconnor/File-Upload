2fe1521d6a06947ce3fcafbc24d66f47dc11310f
Jason <jasonpconn@gmail.com>
2020-09-05 09:20:54 -0400

    Created Post Model and Connected Databse

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 
+const connect = require('./config/db')
 const postRouter = require('./src/routes/post')
 
 const app = express()
 
+// connect to db
+connect
+
+// routes
 app.use('/', postRouter)
 
 app.listen(80, () => {
---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://localhost:27017/file'
+
+const connect = async () => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true
+    }).then(() => {
+      console.log('Connected to MongoDB.')
+    })
+  } catch(error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+module.exports = connect()
